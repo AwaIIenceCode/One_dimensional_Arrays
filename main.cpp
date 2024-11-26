657fe9624a49ca7b0d506e03f905489dc31442ed
Oleh Skrypnyk-Zakrevskiy <awallencecode@gmail.com>
2024-11-26 14:32:34 +0200

    ex 2 is done

--- main.cpp
+++ main.cpp
@@ -4,28 +4,13 @@ using namespace std;
 
 int main()
 {
-   int const SIZE = 6;
-   double arr[SIZE];
-   double company_income = 0;
+    int const SIZE = 10;
+    int arr[SIZE]{18,61,32,5,-4,8,69,23,37,14};
 
-   for (int i = 0; i < SIZE; i++)
-   {
-       cout << "Enter the firm's revenue for month " << (i + 1) << " ->" << endl;
-       cin >> arr[i];
-
-       while (arr[i] < 0)
-       {
-           cout << "Revenue cannot be negative. Please re-enter -> " << endl;
-           cin >> arr[i];
-       }
-   }
-//I can put it in one loop, but it will be better for code readability, although I don't know about optimization.
-   for (int i = 0; i < SIZE; i++)
-   {
-       company_income += arr[i];
-   }
-
-    cout << "\nTotal company income for 6 months: " << company_income << endl;
+    for (int i = SIZE; i--;)
+    {
+        cout << arr[i] << " ";
+    }
 
     return 0;
 }
