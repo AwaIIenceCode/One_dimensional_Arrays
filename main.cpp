1fc123ac728ef03ab2159820a27c289f762ad4f5
Oleh Skrypnyk-Zakrevskiy <awallencecode@gmail.com>
2024-11-27 18:08:28 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,64 +1,57 @@
+//В одновимірному масиві, що складається з N дійсних чисел обчислити:
+//
+//Суму від'ємних елементів.
+//Добуток елементів, що знаходяться між min і max елементами.
+//Добуток елементів з парними номерами.
+//Суму елементів, що знаходяться між першим і останнім від'ємними елементами.
+
 #include <iostream>
-#include <limits>
 
 using namespace std;
 
 int main()
 {
-    int const SIZE = 12;
+    srand(time(NULL));
+
+    int const SIZE = 15;
     double arr[SIZE];
-    int start_diapazone, finish_diapazone;
+    double negative_sum = 0;
+
+    cout << "\nYour list of random action numbers -> \n[ ";
 
     for (int i = 0; i < SIZE; i++)
     {
-        cout << "Enter the profit for the month " << (i + 1) << " -> " <<  endl;
-        cin >> arr[i];
-
-        while (cin.fail() || arr[i] <= 0)
+        if (rand() % 2 == 0)
         {
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            cout << "Invalid input. \nEnter positive profit for the month -> " << endl;
-            cin >> arr[i];
+            arr[i] = rand() % 175 - 75;
+        }
+        else
+        {
+            arr[i] = (rand() % 175 - 75) + static_cast<double>(rand() % 100) / 100.0;
+        }
+
+        cout << arr[i] << "  ";
+    }
+
+    cout << "]" << endl;
+
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        if (arr[i] < 0)
+        {
+            negative_sum += arr[i];
         }
     }
 
-    do
-    {
-        cout << "Enter the start month (1-12) -> ";
-        cin >> start_diapazone;
+    cout << "\nSum of negative numbers: " << negative_sum << endl;
 
-        cout << "Enter the end month (1-12) -> ";
-        cin >> finish_diapazone;
 
-        if (start_diapazone < 1 || start_diapazone > SIZE || finish_diapazone < 1 || finish_diapazone > SIZE || start_diapazone > finish_diapazone)
-        {
-            cout << "Invalid range! Please re-enter.\n";
-        }
-    } while (start_diapazone < 1 || start_diapazone > SIZE || finish_diapazone < 1 || finish_diapazone > SIZE || start_diapazone > finish_diapazone);
 
-    double max_value = arr[start_diapazone - 1];
-    double min_value = arr[start_diapazone - 1];
-    int max_month = start_diapazone;
-    int min_month = start_diapazone;
 
-    for (int i = start_diapazone; i <= finish_diapazone; i++)
-    {
-        if (arr[i - 1] > max_value)
-        {
-            max_value = arr[i - 1];
-            max_month = i;
-        }
 
-        if (arr[i - 1] < min_value)
-        {
-            min_value = arr[i - 1];
-            min_month = i;
-        }
-    }
 
-        cout << "\nMaximum profit: " << max_value << " in month " << max_month << endl;
-        cout << "Minimum profit: " << min_value << " in month " << min_month << endl;
 
-        return 0;
+
+    return 0;
 }
